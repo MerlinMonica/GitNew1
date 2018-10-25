6128f9253f5b21f766fbe6b1a2fcbe8c6e5bb7e2
MerlinMonica <n.merlinmonica@gmail.com>
2018-10-25 23:44:03 +0530

    Testing

---
+++ org/git/GitNew1.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class GitNew1 
+{
+public static void main(String[] args) 
+{
+System.out.println("Welcome To Git");
+}
+public void create()
+{
+	System.out.println("Testing");
+}
+}
+
