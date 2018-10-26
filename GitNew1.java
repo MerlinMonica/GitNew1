de5f73a7f8d790e758ccd98b3a095decfb30411d
MerlinMonica <44445860+MerlinMonica@users.noreply.github.com>
2018-10-26 20:35:09 +0530

    Add files via upload

---
+++ GitNew1.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class GitNew1 
+{
+public static void main(String[] args) 
+{
+System.out.println("Welcome To Git");
+GitNew1 T=new GitNew1();
+T.create();
+}
+public void create()
+{
+	System.out.println("Testing");
+}
+public void Test()
+{
+	System.out.println("Modify Test");
+}
+}
+
